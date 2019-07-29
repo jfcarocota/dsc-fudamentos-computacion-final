e133e3109a9c092c6430bd70c67bafde8695f3a5
jfcarocota <jfcarocota@gmail.com>
2019-07-29 00:27:50 -0700

    Estados ejercicio 1 generados

--- src/ejercicio-1/main.cc
+++ src/ejercicio-1/main.cc
@@ -80,9 +80,48 @@ int main()
         std::getline(in, data);
         PrintString("Data cargada, pulse cualquier tecla para continuar...");
         std::getchar();
+        
+        const char* datachar = data.data();
         for(int i = 0; i < data.size(); i++)
         {
             std::cout << data[i] << std::endl;
+            switch(data[i])
+            {
+                case '(' :
+                    break;
+                case ')' :
+                    break;
+                case '+' :
+                    break;
+                case '*' :
+                    break;
+                case '{' :
+                    break;
+                case '}' :
+                    break;
+                case ',' :
+                    break;
+                case '0' :
+                    break;
+                case '1' :
+                    break;
+                case '2' :
+                    break;
+                case '3' :
+                    break;
+                case '4' :
+                    break;
+                case '5' :
+                    break;
+                case '6' :
+                    break;
+                case '7' :
+                    break;
+                case '8' :
+                    break;
+                case '9' :
+                    break;
+            }
         }
     } 
     else
